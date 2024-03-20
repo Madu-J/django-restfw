b58d1b7ec2cf3d1a8fcf0d78382ea2f129288cd2
Madu-J Omenka <madu.omenka@hotmail.com>
2024-03-20 16:56:30 +0000

    Updated PostSerializer

--- post/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from post import views
-
-urlspatterns = [
-    path('post/', views.PostList.as_view()),
-]
--- posts/serializers.py
+++ posts/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
-from .models import Post
+from posts.models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
-    owner = serializers.ForeignKey(User, on_delete=models.CASCADE)
+    owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
     def get_is_owner(self, obj):
         request = self.context['request']
